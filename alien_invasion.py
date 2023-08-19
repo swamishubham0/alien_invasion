bfe25a7b0c46bcf4cd8193f82ce736aefd3a9fd9
Shubham <swamishubham0@gmail.com>
2023-08-19 21:42:22 +0530

    refactored code

--- alien_invasion.py
+++ alien_invasion.py
@@ -43,18 +43,26 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
-            
+                self._check_keydown_events()
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_eveents()
+            elif event.type == pygame.K_q:
+                sys.exit()
+    
+    def _check_keyup_eveents(self, event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _check_keydown_events(self, event):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
