5306065afde3a7f5405126087632eb4e01b2ada3
Shubham <swamishubham0@gmail.com>
2023-08-19 23:07:35 +0530

    Created Fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,7 +22,7 @@ class AlienInvasion:
 
         # Create a display window on whcih we will draw all the games graphical elements
         self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.scree_height))
+            (self.settings.screen_width, self.settings.screen_height))
 
         # for fullscreen game:
         # self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
@@ -45,18 +45,25 @@ class AlienInvasion:
         """Create the fleet of aliens"""
         # Make an alien
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
+        
 
-        current_x = alien_width
-        while current_x < (self.settings.screen_width - 2 * alien_width):
-            self._create_alien(current_x)
-            current_x += 2 * alien_width
+        current_x , current_y = alien_width, alien_height
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
+            
+            #Finished a row; reset x value, and increment y value
+            current_x = alien_width
+            current_y +=  2 * alien_height
 
-    def _create_alien(self, x_position):
-        """Create an alien and place it in the row."""
+    def _create_alien(self, x_position, y_position):
+        """Create an alien and place it in the fleet."""
         new_alien = Alien(self)
         new_alien.x = x_position
         new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
         self.aliens.add(new_alien)
 
 
