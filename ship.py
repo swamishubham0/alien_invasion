ddb1b273ce8cee50f2f2c76a45a39d9d0bd464a6
Shubham <swamishubham0@gmail.com>
2023-08-19 21:10:02 +0530

    refactored code

--- ship.py
+++ ship.py
@@ -7,7 +7,7 @@ class Ship:
         """Initialize the ship and set its starting position"""
 
         self.screen = ai_game.screen
-        self.screen_rect = ai_game.screen.geet_rect()
+        self.screen_rect = ai_game.screen.get_rect()
 
         # load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
