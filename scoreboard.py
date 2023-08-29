cd4eb8408c574856a885e154b181bf0c4c38bcaf
Shubham <swamishubham0@gmail.com>
2023-08-29 15:16:36 +0530

    dynamic scoring

--- scoreboard.py
+++ scoreboard.py
@@ -8,10 +8,10 @@ class Scoreboard:
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
         self.settings = ai_game.settings
-        self.stats - ai_game.stats
+        self.stats = ai_game.stats
 
         # Font settings for scoring information
-        self.text_color(30, 30, 30)
+        self.text_color=(30, 30, 30)
         self.font = pygame.font.SysFont(None, 48)
 
         # Prepare the initial score image
--- settings.py
+++ settings.py
@@ -23,6 +23,9 @@ class Settings:
         #How quickly the game speeds up
         self.speedup_scale = 1.1
 
+        # How quickly the alien point value increase
+        self.score_scale = 1.5
+
         self.initialize_dynamic_settings()
 
 
@@ -46,3 +49,6 @@ class Settings:
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
+        
