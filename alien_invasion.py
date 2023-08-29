7b92eae1637d0c93086ae371bcc7150c663fb6bc
Shubham <swamishubham0@gmail.com>
2023-08-29 15:55:40 +0530

    high score completed

--- alien_invasion.py
+++ alien_invasion.py
@@ -171,14 +171,19 @@ class AlienInvasion:
         if collisions:
             for aliens in collisions.values():
                 self.stats.score +=self.settings.alien_points * len(aliens)
-                
+
             self.sb.prep_score()
+            self.sb.check_high_score()
 
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # Increase level.
+            self.stats.level += 1
+            self.sb.prep_level()
 
 
     def _check_events(self):
@@ -205,6 +210,7 @@ class AlienInvasion:
             # Reset the game statistics
             self.stats.reset_stats()
             self.sb.prep_score()
+            self.sb.prep_level()
             self.game_active = True
 
             #Get rid of any remaining bullets and alien
